440e4711449a94bbdd1a3f5fd609028984d8c283
diggerdu <diggerdu@hotmail.com>
2016-12-19 13:06:23 +0800

    nothing

--- crude_lstm/lstm.py
+++ crude_lstm/lstm.py
@@ -7,11 +7,12 @@ import pickle
 import time
 
 data_path = "../data/"
+logdir = './logs'
 # Parameters
 learning_rate = 0.0001
 training_iters = 10000000000
 batch_size = 1
-display_step = 100
+display_step = 12
 milestone = 0.78
 
 # Network Parameters
@@ -56,9 +57,19 @@ def RNN(x, weights, biases):
 
 pred = RNN(x, weights, biases)
 
+#measure accurancy
+accurancy = tf.reduce_mean(tf.cast(tf.equal(tf.round(pred), y), tf.float32))
+
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(pred, y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+#summary operation
+loss_sum = tf.summary.scalar('loss', cost)
+accurancy_sum = tf.summary.scalar('accurancy', accurancy)
+#summary_writer = tf.train.SummaryWriter(logdir, sess)
+
 
 # Evaluate model
 #correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
@@ -85,6 +96,7 @@ validate_best = 0
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
+    writer = tf.train.SummaryWriter(logdir, sess.graph)
     step = 1
     # Keep training until reach max iterations
     while step * batch_size < training_iters:
@@ -92,29 +104,23 @@ with tf.Session() as sess:
         batch_x = train_data[idx]
         batch_y = train_label[idx]
         embark = time.time()
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+        _, loss_sum_ = sess.run([optimizer, loss_sum], feed_dict={x: batch_x, y: batch_y})
+        writer.add_summary(loss_sum_, global_step = step)
         print (time.time() - embark,"s per batch")
         if step % display_step == 0:
-            pred_data = sess.run(pred, feed_dict={x: eva_data[:50], y: eva_label[:50]})
-            discrete_output = np.where(pred_data > 0.5, 1, 0)
-            right = sum(discrete_output == batch_y) 
-            print ("after %d epoch, thus far training accurancy is : %f" %(step, right / 50))
-            if  right / batch_size > milestone:
-                saver.save(sess, "./checkpoint/model.ckpt")
-                pred_data = sess.run(pred, feed_dict={x: eva_data, y: eva_label})
-                discrete_output = np.where(pred_data > 0.5, 1, 0)
-                right = sum(discrete_output == eva_label)
+            accurancy_sum_, accurancy_ = sess.run([accurancy_sum, accurancy], feed_dict={x: eva_data, y: eva_label})
+            writer.add_summary(accurancy_sum_, global_step = step)
+            print ("after %d epoch, thus far training accurancy is : %f" %(step, accurancy_))
+            if  accurancy_ > milestone:
+                saver.save(sess, "./checkpoint/model.ckpt") 
+                accurancy_ = sess.run(accurancy, feed_dict={x: eva_data, y: eva_label})
                 
-                print ("####thus far validate accurancy is : %f #####" %(right / eva_data.shape[0]))
-                if right / eva_data.shape[0] > validate_best:
-                    validate_best = right / eva_data.shape[0]
+                print ("####thus far validate accurancy is : %f #####" %(accurancy_))
+                if accurancy_ > validate_best:
+                    validate_best = accurancy_
                     if validate_best > 0.86:
                         saver.save(sess, "./checkpoint/extraoridinary.ckpt")
                 print ("####thus far best validate accurancy is : %f #####" %(validate_best))
-                wrong_item = np.argwhere((discrete_output == eva_label) == False)
-                wrong_item = wrong_item[:,0]
-                np.save("wrong_item.npy", wrong_item)
-                np.save("prob.npy", pred_data)
                 np.save("real.npy", eva_label)
         step += 1
     print("Optimization Finished!")
