43873b18eccf97d77b4e5e905db9409203842197
diggerdu <diggerdu@hotmail.com>
2016-12-20 23:15:34 +0800

    uilts

--- lstm_ctc/ultis.py
+++ lstm_ctc/ultis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import string
 
 def sparse_tuple_from(sequences, n_classes, dtype=np.int32):
 		
@@ -7,3 +8,24 @@ def sparse_tuple_from(sequences, n_classes, dtype=np.int32):
 	val = np.asarray([v for k, v in np.ndenumerate(sequences) if v > 0])
 	shape = np.asarray([sequences.shape[0], n_classes])
 	return idx, val, shape
+
+def dense_from_sparse(sparse):
+    idx, val, shape = sparse
+    dense = np.zeros(shape)
+    for i in range(idx.shape[0]):
+        dense[tuple(idx[i])] = val[i]
+    return dense
+
+def decode_from_arr(arr):
+    decode_dict = dict(enumerate(string.lowercase))
+    assert len(arr.shape) == 2
+    arr = arr.tolist()
+    item_list = list()
+    for item in arr:
+        item = ''.join(map(lambda x:decode_dict[x], item))
+        item_list.append(item)
+    return item_list
+
+
+
+
