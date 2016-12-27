b0f5023286ea823cefd07056505af83d05b8517c
diggerdu <diggerdu@hotmail.com>
2016-12-27 18:17:43 +0000

    fix mistake in sparse_from utils

--- lstm_ctc/ultis.py
+++ lstm_ctc/ultis.py
@@ -5,5 +5,5 @@ def sparse_tuple_from(sequences, n_classes, dtype=np.int32):
 
 	idx = np.vstack([k for k, v in np.ndenumerate(sequences) if v > 0])
 	val = np.asarray([v for k, v in np.ndenumerate(sequences) if v > 0])
-	shape = np.asarray([sequences.shape[0], n_classes])
+	shape = np.asarray(sequences.shape)
 	return idx, val, shape
