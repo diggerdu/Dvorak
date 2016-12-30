e94002247751bdcd6e07c13d9411204ed34d03a6
diggerdu <diggerdu@hotmail.com>
2016-12-30 13:31:30 +0800

    deploy

---
+++ deploy/tcp.py
@@ -0,0 +1,20 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 7251
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connection address:', addr
+
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: 
+        print 'no data'
+        break
+    print data
+    conn.send(data)
+conn.close()
+
